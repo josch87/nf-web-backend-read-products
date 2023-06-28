c0e56775ecbb5d5b75fa0ab91a9caf71f9f6d29b
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-28 16:32:58 +0200

    read data from db

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,13 +1,23 @@
-import { products } from "../../../lib/products";
+import dbConnect from "../../../db/connect";
+import Product from "../../../db/models/Products";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  await dbConnect();
 
-  if (!product) {
-    return response.status(404).json({ status: "Not Found" });
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
+    return response.status(200).json(product);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
   }
 
-  response.status(200).json(product);
+  // const product = products.find((product) => product.id === id);
+
+  // if (!product) {
+  //   return response.status(404).json({ status: "Not Found" });
+  // }
+
+  // response.status(200).json(product);
 }
