e5efbda9c00f025a69a66016c45622c20ecd42e6
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-28 16:40:07 +0200

    delete comments

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -12,12 +12,4 @@ export default async function handler(request, response) {
   } else {
     return response.status(405).json({ message: "Method not allowed" });
   }
-
-  // const product = products.find((product) => product.id === id);
-
-  // if (!product) {
-  //   return response.status(404).json({ status: "Not Found" });
-  // }
-
-  // response.status(200).json(product);
 }
