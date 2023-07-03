201bf1cf1a89d844752b5d09cc9161c511414650
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-07-03 08:54:08 +0200

    send put request and insert db value on form

--- components/Product/index.js
+++ components/Product/index.js
@@ -2,12 +2,36 @@ import useSWR from "swr";
 import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
+import { useState } from "react";
+import { StyledButton } from "../Button/Button.styled";
+import ProductForm from "../ProductForm";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleUpdateProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  const [isEditMode, setIsEditMode] = useState(false);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -25,6 +49,21 @@ export default function Product() {
         Price: {data.price} {data.currency}
       </p>
       <StyledLink href="/">Back to all</StyledLink>
+      <StyledButton
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Update
+      </StyledButton>
+      {isEditMode ? (
+        <ProductForm
+          onSubmit={handleUpdateProduct}
+          heading="Update this fish"
+          product={data}
+        />
+      ) : null}
     </ProductCard>
   );
 }
--- components/ProductForm/index.js
+++ components/ProductForm/index.js
@@ -1,47 +1,57 @@
 import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
 import { StyledButton } from "../Button/Button.styled";
-import useSWR from "swr";
+import { useState } from "react";
 
-export default function ProductForm() {
-  const { mutate } = useSWR("/api/products");
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const productData = Object.fromEntries(formData);
-
-    const response = await fetch("/api/products/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productData),
-    });
-
-    if (response.ok) {
-      mutate();
-    }
-  }
+export default function ProductForm({ onSubmit, heading, product }) {
+  const [nameValue, setNameValue] = useState(product?.name);
+  const [descriptionValue, setDescriptionValue] = useState(
+    product?.description
+  );
+  const [priceValue, setPriceValue] = useState(product?.price);
+  const [currencyValue, setCurrencyValue] = useState(product?.currency);
 
   return (
-    <StyledForm onSubmit={handleSubmit}>
-      <StyledHeading>Add a new Fish</StyledHeading>
+    <StyledForm onSubmit={onSubmit}>
+      <StyledHeading>{heading}</StyledHeading>
       <StyledLabel htmlFor="name">
         Name:
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={nameValue}
+          onChange={(event) => setNameValue(event.target.value)}
+        />
       </StyledLabel>
       <StyledLabel htmlFor="description">
         Description:
-        <input type="text" id="description" name="description" />
+        <input
+          type="text"
+          id="description"
+          name="description"
+          value={descriptionValue}
+          onChange={(event) => setDescriptionValue(event.target.value)}
+        />
       </StyledLabel>
       <StyledLabel htmlFor="price">
         Price:
-        <input type="number" id="price" name="price" min="0" />
+        <input
+          type="number"
+          id="price"
+          name="price"
+          min="0"
+          value={priceValue}
+          onChange={(event) => setPriceValue(event.target.value)}
+        />
       </StyledLabel>
       <StyledLabel htmlFor="currency">
         Currency:
-        <select id="currency" name="currency">
+        <select
+          id="currency"
+          name="currency"
+          value={currencyValue}
+          onChange={(event) => setCurrencyValue(event.target.value)}
+        >
           <option value="EUR">EUR</option>
           <option value="USD">USD</option>
           <option value="GBP">GBP</option>
